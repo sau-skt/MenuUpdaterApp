d5011ad4a2fe26241b81eb6ce523563bf339ce75
sau-skt <saurabhchatterjee18@gmail.com>
2023-05-22 17:44:32 +0530

    v1.0

--- app/src/main/java/com/example/myapplication/GenerateReportActivity.java
+++ app/src/main/java/com/example/myapplication/GenerateReportActivity.java
@@ -2,11 +2,14 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,8 +23,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class GenerateReportActivity extends AppCompatActivity {
@@ -31,6 +39,7 @@ public class GenerateReportActivity extends AppCompatActivity {
     String username;
     Button downloadreport;
     DatabaseReference cxorderreference;
+    StringBuilder csvData = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,9 +98,9 @@ public class GenerateReportActivity extends AppCompatActivity {
                         LocalDate startDate = LocalDate.parse(startdatetv.getText().toString(), dateFormatter);
                         LocalDate endDate = LocalDate.parse(enddatetv.getText().toString(), dateFormatter);
 
+                        csvData.append("InvoiceDate, InvoiceNumber, SubTotal, Total, OrderStatus\n");
+
                         if (startDate.isBefore(endDate) || startDate.isEqual(endDate)) {
-                            for (LocalDate date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
-                                String formattedDate = date.format(output);
                                 cxorderreference.addListenerForSingleValueEvent(new ValueEventListener() {
                                     @Override
                                     public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -101,10 +110,46 @@ public class GenerateReportActivity extends AppCompatActivity {
                                                 @Override
                                                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                                                         String invoiceDate = snapshot.child("invoicedate").getValue(String.class);
+                                                    for (LocalDate date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
+                                                        String formattedDate = date.format(output);
                                                         if (invoiceDate != null && invoiceDate.equals(String.valueOf(formattedDate))) {
-                                                            Log.e("QWE", String.valueOf(invoiceDate));
+                                                            String invsubtotal = snapshot.child("invoicesubtotal").getValue(String.class);
+                                                            String invtotal = snapshot.child("invoicetotal").getValue(String.class);
+                                                            String ordstatus = snapshot.child("order_status").getValue(String.class);
+                                                            csvData.append(invoiceDate + ",");
+                                                            csvData.append(inno + ",");
+                                                            csvData.append(invsubtotal + ",");
+                                                            csvData.append(invtotal + ",");
+                                                            csvData.append(ordstatus + "\n");
                                                         }
+                                                    }
+                                                    File dir = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                                                    File file = new File(dir, "report.csv");
 
+                                                    if (!file.exists()){
+                                                        try {
+                                                            file.createNewFile();
+                                                        } catch (IOException e) {
+                                                            throw new RuntimeException(e);
+                                                        }
+                                                    }
+
+                                                    try {
+                                                        FileOutputStream outputStream = new FileOutputStream(file);
+                                                        outputStream.write(csvData.toString().getBytes());
+                                                        outputStream.close();
+                                                        Toast.makeText(GenerateReportActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                                                        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                                                        emailIntent.setType("text/csv");
+                                                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Report File");
+                                                        emailIntent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(GenerateReportActivity.this,BuildConfig.APPLICATION_ID + ".fileprovider", file));
+                                                        emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                                                        startActivity(Intent.createChooser(emailIntent, "Send Email..."));
+                                                    } catch (IOException e) {
+                                                        e.printStackTrace();
+                                                    }
                                                 }
 
                                                 @Override
@@ -120,7 +165,6 @@ public class GenerateReportActivity extends AppCompatActivity {
 
                                     }
                                 });
-                            }
                         } else {
                             Toast.makeText(GenerateReportActivity.this, "Start date cannot be after end date", Toast.LENGTH_SHORT).show();
                         }
